34f1ba4158006d562bcd8ca24dc92b9e9cb877ed
Jan Matter <jan.matter@outlook.com>
2025-03-24 22:41:20 +0100

    fixed error

--- modules/cert-manager/main.tf
+++ modules/cert-manager/main.tf
@@ -1,19 +1,17 @@
 resource "kubernetes_manifest" "letsencrypt" {
-  provider = kubernetes.default
-  for_each = toset(var.cert_issuers)
 
   manifest = {
     "apiVersion" = "cert-manager.io/v1"
     "kind" = "ClusterIssuer"
     "metadata" = {
-      "name" = each.value
+      "name" = "letsencrypt-prod" # Replace with your desired name
     }
     "spec" = {
       "acme" = {
         "server" = "https://acme-v02.api.letsencrypt.org/directory"
         "email" = var.cert_email
         "privateKeySecretRef" = {
-          "name" = each.value
+          "name" = "letsencrypt-prod" # Replace with your desired secret name
         }
         "solvers" = [
           {
--- modules/cert-manager/provider.tf
+++ modules/cert-manager/provider.tf
@@ -1,8 +1,6 @@
-provider "helm" {
-  kubernetes {
-    host                   = var.k8s_host
-    client_certificate     = base64decode(var.k8s_client_certificate)
-    client_key             = base64decode(var.k8s_client_key)
-    cluster_ca_certificate = base64decode(var.k8s_cluster_ca_certificate)
-  }
+provider "kubernetes" {
+  host                   = var.k8s_host
+  client_certificate     = base64decode(var.k8s_client_certificate)
+  client_key             = base64decode(var.k8s_client_key)
+  cluster_ca_certificate = base64decode(var.k8s_cluster_ca_certificate)
 }
