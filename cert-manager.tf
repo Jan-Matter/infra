33036b3ddd4ea03fb042395138cde0511889dfd6
Jan Matter <jan.matter@outlook.com>
2025-03-24 23:43:19 +0100

    deploy

--- cert-manager.tf
+++ cert-manager.tf
@@ -4,7 +4,6 @@ locals {
 
 module "cert-manager" {
     source = "./modules/cert-manager"
-    
     cert_email = local.cert_email
     k8s_host = local.k8s_host
     k8s_client_certificate = local.k8s_client_certificate
