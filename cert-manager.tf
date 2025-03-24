8a857a3a6443bfd429a240877e6286425abfa4e3
Jan Matter <jan.matter@outlook.com>
2025-03-25 00:03:28 +0100

    try fix dep issue

--- cert-manager.tf
+++ cert-manager.tf
@@ -2,10 +2,6 @@ locals {
     cert_email = "jan.matter@dataplumbers.ch"
 }
 
-resource "null_resource" "wait_for_kubeconfig" {
-  depends_on = [module.kube-hetzner]
-}
-
 module "cert-manager" {
     source = "./modules/cert-manager"
     cert_email = local.cert_email
@@ -13,7 +9,5 @@ module "cert-manager" {
     k8s_client_certificate = local.k8s_client_certificate
     k8s_client_key = local.k8s_client_key
     k8s_cluster_ca_certificate = local.k8s_cluster_ca_certificate
-
-    depends_on = [null_resource.wait_for_kubeconfig]
     
 }
--- locals.tf
+++ locals.tf
@@ -1,9 +1,8 @@
 
 
 locals {
-    kubeconfig = yamldecode(module.kube-hetzner.kubeconfig)
-    k8s_host = local.kubeconfig["clusters"][0]["cluster"]["server"]
-    k8s_client_certificate = base64decode(local.kubeconfig["users"][0]["user"]["client-certificate-data"])
-    k8s_client_key = base64decode(local.kubeconfig["users"][0]["user"]["client-key-data"])
-    k8s_cluster_ca_certificate = base64decode(local.kubeconfig["clusters"][0]["cluster"]["certificate-authority-data"])
+    k8s_host = yamldecode(module.kube-hetzner.kubeconfig)["clusters"][0]["cluster"]["server"]
+    k8s_client_certificate = base64decode(yamldecode(module.kube-hetzner.kubeconfig)["users"][0]["user"]["client-certificate-data"])
+    k8s_client_key = base64decode(yamldecode(module.kube-hetzner.kubeconfig)["users"][0]["user"]["client-key-data"])
+    k8s_cluster_ca_certificate = base64decode(yamldecode(module.kube-hetzner.kubeconfig)["clusters"][0]["cluster"]["certificate-authority-data"])
 }
