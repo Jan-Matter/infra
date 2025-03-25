f40a0b94dbe0b1e484b5be9f37efe8c32c58e7f3
Jan Matter <jan.matter@outlook.com>
2025-03-25 06:12:11 +0100

    add dep directly

--- cert-manager.tf
+++ cert-manager.tf
@@ -5,9 +5,9 @@ locals {
 module "cert-manager" {
     source = "./modules/cert-manager"
     cert_email = local.cert_email
-    k8s_host = local.k8s_host
-    k8s_client_certificate = local.k8s_client_certificate
-    k8s_client_key = local.k8s_client_key
-    k8s_cluster_ca_certificate = local.k8s_cluster_ca_certificate
+    k8s_host = yamldecode(module.kube-hetzner.kubeconfig)["clusters"][0]["cluster"]["server"]
+    k8s_client_certificate = base64decode(yamldecode(module.kube-hetzner.kubeconfig)["users"][0]["user"]["client-certificate-data"])
+    k8s_client_key = base64decode(yamldecode(module.kube-hetzner.kubeconfig)["users"][0]["user"]["client-key-data"])
+    k8s_cluster_ca_certificate = base64decode(yamldecode(module.kube-hetzner.kubeconfig)["clusters"][0]["cluster"]["certificate-authority-data"])
     
 }
