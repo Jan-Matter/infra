f222b363a0976135e9fac3537887c0a564771c4f
Jan Matter <jan.matter@outlook.com>
2025-03-25 09:15:36 +0100

    apply ingress separately

--- deployments/kubernetes_controllers/argocd.tf
+++ deployments/kubernetes_controllers/argocd.tf
@@ -1,5 +1,6 @@
 locals {
     argocd_version = "7.8.13"
+    host = "argocd.dataplumbers.ch"
 }
 
 module "argocd" {
@@ -13,4 +14,51 @@ module "argocd" {
     k8s_client_key = local.k8s_client_key
     k8s_cluster_ca_certificate = local.k8s_cluster_ca_certificate
     
+}
+
+#further add ingress
+
+resource "kubernetes_manifest" "argocd_ingress" {
+  manifest = {
+    apiVersion = "networking.k8s.io/v1"
+    kind = "Ingress"
+    metadata = {
+      name = "argocd-ingress"
+      namespace = "argocd"
+      annotations = {
+        "kubernetes.io/ingress.class" = "nginx"
+        "cert-manager.io/cluster-issuer" = "letsencrypt-prod"
+        "nginx.ingress.kubernetes.io/force-ssl-redirect" = "true"
+      }
+    }
+    spec = {
+      tls = [
+        {
+          hosts = [local.host]
+          secretName = "argocd-tls-secret"
+        }
+      ]
+      rules = [
+        {
+          host = local.host
+          http = {
+            paths = [
+              {
+                path = "/"
+                pathType = "Prefix"
+                backend = {
+                  service = {
+                    name = "argocd-server"
+                    port = {
+                      number = 80
+                    }
+                  }
+                }
+              }
+            ]
+          }
+        }
+      ]
+    }
+  }
 }
