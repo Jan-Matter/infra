47285e760bd3903fa4fb17d696a2273d8b2d8dbb
Jan Matter <jan.matter@outlook.com>
2025-03-25 08:42:43 +0100

    deploy argocd

--- deployments/kubernetes_controllers/argocd.tf
+++ deployments/kubernetes_controllers/argocd.tf
@@ -8,9 +8,9 @@ module "argocd" {
     values_path = "./helm_values/argocd.yaml"
     
     argocd_version = local.argocd_version
-    k8s_host = local.k8s_host
-    k8s_client_certificate = local.k8s_client_certificate
-    k8s_client_key = local.k8s_client_key
-    k8s_cluster_ca_certificate = local.k8s_cluster_ca_certificate
+    k8s_host = var.k8s_host
+    k8s_client_certificate = var.k8s_client_certificate
+    k8s_client_key = var.k8s_client_key
+    k8s_cluster_ca_certificate = var.k8s_cluster_ca_certificate
     
 }
--- deployments/kubernetes_infra/locals.tf
+++
@@ -1,7 +0,0 @@
-
-locals {
-    k8s_host = yamldecode(module.kube-hetzner.kubeconfig)["clusters"][0]["cluster"]["server"]
-    k8s_client_certificate = base64decode(yamldecode(module.kube-hetzner.kubeconfig)["users"][0]["user"]["client-certificate-data"])
-    k8s_client_key = base64decode(yamldecode(module.kube-hetzner.kubeconfig)["users"][0]["user"]["client-key-data"])
-    k8s_cluster_ca_certificate = base64decode(yamldecode(module.kube-hetzner.kubeconfig)["clusters"][0]["cluster"]["certificate-authority-data"])
-}
