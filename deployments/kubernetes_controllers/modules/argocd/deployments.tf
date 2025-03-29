b6ebe73691a56a1254f3d68950662ca86a242f89
Jan Matter <jan.matter@outlook.com>
2025-03-29 15:08:28 +0100

    deinstall airflow

--- deployments/kubernetes_controllers/modules/argocd/deployments.tf
+++ deployments/kubernetes_controllers/modules/argocd/deployments.tf
@@ -1,44 +1,47 @@
-# resource "kubernetes_manifest" "airflow-prod" {
-#   manifest = {
-#     "apiVersion" = "argoproj.io/v1alpha1"
-#     "kind"       = "Application"
-#     "metadata" = {
-#       "name"      = "airflow"
-#       "namespace" = "argocd"
-#     }
-#     "spec" = {
-#       "project" = "default"
-#       "sources" = [
-#         {
-#           "chart": "airflow",
-#           "repoURL": "https://airflow.apache.org",
-#           "targetRevision": "1.15.0",
-#           "helm": {
-#             "releaseName": "airflow-prod",
-#             "valueFiles" = [
-#               "$values/helm-values/airflow-prod/values.yaml"
-#             ]
-#           }
-#         },
-#         {
-#           "repoURL" = "git@github.com:Jan-Matter/airflow.git",
-#           "targetRevision" = "main",
-#           "ref" = "values"
-#         }
-#       ]
-#       "destination" = {
-#         "server": "https://kubernetes.default.svc",
-#         "namespace": "airflow"
-#       }
-#       "syncPolicy" = {
-#         "automated" = {
-#           "prune"    = true
-#           "selfHeal" = true
-#         },
-#         "syncOptions" = [
-#           "CreateNamespace=true"
-#         ]
-#       }
-#     }
-#   }
-# }
+resource "kubernetes_manifest" "airflow-prod" {
+  manifest = {
+    "apiVersion" = "argoproj.io/v1alpha1"
+    "kind"       = "Application"
+    "metadata" = {
+      "name"      = "airflow"
+      "namespace" = "argocd"
+      "finalizers" = [
+        "resources-finalizer.argocd.argoproj.io"
+      ]
+    }
+    "spec" = {
+      "project" = "default"
+      "sources" = [
+        {
+          "chart": "airflow",
+          "repoURL": "https://airflow.apache.org",
+          "targetRevision": "1.15.0",
+          "helm": {
+            "releaseName": "airflow-prod",
+            "valueFiles" = [
+              "$values/helm-values/airflow-prod/values.yaml"
+            ]
+          }
+        },
+        {
+          "repoURL" = "git@github.com:Jan-Matter/airflow.git",
+          "targetRevision" = "main",
+          "ref" = "values"
+        }
+      ]
+      "destination" = {
+        "server": "https://kubernetes.default.svc",
+        "namespace": "airflow"
+      }
+      "syncPolicy" = {
+        "automated" = {
+          "prune"    = true
+          "selfHeal" = true
+        },
+        "syncOptions" = [
+          "CreateNamespace=true"
+        ]
+      }
+    }
+  }
+}
