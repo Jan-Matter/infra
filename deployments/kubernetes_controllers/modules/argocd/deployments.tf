6d2fd47a086f96664847b8d056ab03df6d58238e
Jan Matter <jan.matter@outlook.com>
2025-04-08 18:08:44 +0200

    remove spark

--- deployments/kubernetes_controllers/modules/argocd/deployments.tf
+++ deployments/kubernetes_controllers/modules/argocd/deployments.tf
@@ -44,47 +44,47 @@ resource "kubernetes_manifest" "airflow-prod" {
 }
 
 
-resource "kubernetes_manifest" "spark-prod" {
-  manifest = {
-    "apiVersion" = "argoproj.io/v1alpha1"
-    "kind"       = "Application"
-    "metadata" = {
-      "name"      = "spark"
-      "namespace" = "argocd"
-    }
-    "spec" = {
-      "project" = "default"
-      "sources" = [
-        {
-          "chart": "spark-operator",
-          "repoURL": "https://kubeflow.github.io/spark-operator",
-          "targetRevision": "2.1.1",
-          "helm": {
-            "releaseName": "spark-prod",
-            "valueFiles" = [
-              "$values/spark/values.yaml"
-            ]
-          }
-        },
-        {
-          "repoURL" = "git@github.com:Jan-Matter/kubernetes-deployments.git",
-          "targetRevision" = "main",
-          "ref" = "values"
-        }
-      ]
-      "destination" = {
-        "server": "https://kubernetes.default.svc",
-        "namespace": "spark"
-      }
-      "syncPolicy" = {
-        "automated" = {
-          "prune"    = true
-          "selfHeal" = true
-        },
-        "syncOptions" = [
-          "CreateNamespace=true"
-        ]
-      }
-    }
-  }
-}
+# resource "kubernetes_manifest" "spark-prod" {
+#   manifest = {
+#     "apiVersion" = "argoproj.io/v1alpha1"
+#     "kind"       = "Application"
+#     "metadata" = {
+#       "name"      = "spark"
+#       "namespace" = "argocd"
+#     }
+#     "spec" = {
+#       "project" = "default"
+#       "sources" = [
+#         {
+#           "chart": "spark-operator",
+#           "repoURL": "https://kubeflow.github.io/spark-operator",
+#           "targetRevision": "2.1.1",
+#           "helm": {
+#             "releaseName": "spark-prod",
+#             "valueFiles" = [
+#               "$values/spark/values.yaml"
+#             ]
+#           }
+#         },
+#         {
+#           "repoURL" = "git@github.com:Jan-Matter/kubernetes-deployments.git",
+#           "targetRevision" = "main",
+#           "ref" = "values"
+#         }
+#       ]
+#       "destination" = {
+#         "server": "https://kubernetes.default.svc",
+#         "namespace": "spark"
+#       }
+#       "syncPolicy" = {
+#         "automated" = {
+#           "prune"    = true
+#           "selfHeal" = true
+#         },
+#         "syncOptions" = [
+#           "CreateNamespace=true"
+#         ]
+#       }
+#     }
+#   }
+# }
